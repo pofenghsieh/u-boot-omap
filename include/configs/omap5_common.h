7d9b070be315733415882a87455dc1da92f82a81
Ravikumar Kattekola <rk@ti.com>
2014-08-20 20:12:53 +0530

    OMAP: Increase console I/O buffer size
    There are some cases where the environment variables might
    exceed 256 B in length.
    Such an example is setting the bootcmd and bootargs with nested if-then-fi
    loops.
    If the command exceeds the set legth limit then it results in syntax error
    hence increasing the size to 512 B.
    
    Change-Id: I0785ddc54ac71bc174ecb4518adbd8aac86d27e7
    Signed-off-by: Ravikumar Kattekola <rk@ti.com>

--- include/configs/omap5_common.h
+++ include/configs/omap5_common.h
@@ -211,7 +211,7 @@
 
 #define CONFIG_SYS_LONGHELP	/* undef to save memory */
 #define CONFIG_SYS_HUSH_PARSER	/* use "hush" command parser */
-#define CONFIG_SYS_CBSIZE		256
+#define CONFIG_SYS_CBSIZE		512
 /* Print Buffer Size */
 #define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
 					sizeof(CONFIG_SYS_PROMPT) + 16)
