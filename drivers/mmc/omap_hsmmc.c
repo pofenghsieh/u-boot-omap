pofenghsieh/u-boot-omap

commit ce0e0c0c82cebff445c21f10c7776b7b688f82a2

dra7xx: mmc: Enable DDR50 mode for MMC1
1. Enable DDR50 flag in the host driver capabilities mask.

2. Enable in DDR mode in the MMC peripheral(CON[19]) when the MMC driver
is validating DDR bus widths.

    Without this change, the MMC peripheral stays in SDR mode while the
    eMMC device is in DDR mode. This results in the driver treating the
    DDR mode as invalid and falling through to SDR mode.

Change-Id: I7595fea14fc739fd15d1b62f6817b6681d055233
Signed-off-by: Venkateswara Rao Mandela <venkat.mandela@ti.com>

drivers/mmc/omap_hsmmc.c
@@ -717,6 +717,13 @@ static void omap_hsmmc_set_ios(struct mmc *mmc)
 		break;
 	}
 
+	if (mmc->ddr) {
+		writel(readl(&mmc_base->con) | CON_DDR,
+		       &mmc_base->con);
+	} else {
+		writel(readl(&mmc_base->con) & ~CON_DDR,
+		       &mmc_base->con);
+	}
 	/* configure clock with 96Mhz system clock.
 	 */
 	if (mmc->clock != 0) {
@@ -806,6 +813,7 @@ int omap_mmc_init(int dev_index, uint host_caps_mask, uint f_max, int cd_gpio,
 		defined(CONFIG_HSMMC2_8BIT)
 		/* Enable 8-bit interface for eMMC on OMAP4/5 or DRA7XX */
 		host_caps_val |= MMC_MODE_8BIT;
+		host_caps_val |= MMC_MODE_DDR_52MHz;
 #endif
 		break;
 #endif

arch/arm/include/asm/omap_mmc.h
@@ -108,6 +108,7 @@ struct hsmmc {
 #define INDEX(i)			(i << 24)
 #define DATI_MASK			(0x1 << 1)
 #define CMDI_MASK			(0x1 << 0)
+#define CON_DDR                         (0x1 << 19)
 #define DTW_1_BITMODE			(0x0 << 1)
 #define DTW_4_BITMODE			(0x1 << 1)
 #define DTW_8_BITMODE                   (0x1 << 5) /* CON[DW8]*/

drivers/mmc/mmc.c
@@ -1133,6 +1133,12 @@ static int mmc_startup(struct mmc *mmc)
 			if (err)
 				continue;
 
+			if ((extw == EXT_CSD_DDR_BUS_WIDTH_8) ||
+			    (extw == EXT_CSD_DDR_BUS_WIDTH_4))
+				mmc->ddr = 1;
+			else
+				mmc->ddr = 0;
+
 			mmc_set_bus_width(mmc, widths[idx]);
 
 			err = mmc_send_ext_csd(mmc, test_csd);
@@ -1293,6 +1299,13 @@ int mmc_start_init(struct mmc *mmc)
 
 	if (mmc->has_init)
 		return 0;
+
+	/*
+	 * Initialize dual data rate flag to 0 by default.
+	 * This flag will be set based on the host capabilities
+	 * and device capabilities during mmc_startup() call.
+	 */
+	mmc->ddr = 0;
 
 	/* made sure it's not NULL earlier */
 	err = mmc->cfg->ops->init(mmc);

include/mmc.h
@@ -314,6 +314,7 @@ struct mmc {
 	char init_in_progress;	/* 1 if we have done mmc_start_init() */
 	char preinit;		/* start init as early as possible */
 	uint op_cond_response;	/* the response byte from the last op_cond */
+	uint ddr;
 };
 
 int mmc_register(struct mmc *mmc);

commit 2920896368e28842d0e7135ffc4d229ddb65e05f

dra7xx: mmc: Selectively enable DDR_52 mode
DRA7xx platforms require specific IO delay configurations based on the
MMC mode selected. U-boot performs pad config and delay config
only once. This restricts switching MMC modes dynamically based on card
capabilities. Hence, allow only one mode based on user CONFIG.

User needs to explicitly enable the CONFIG option which would set the
appropriate delays as well as enable controller caps.

Change-Id: I115df99fd05f99cb98290a2beea85684172fe1f9
Fixes: ce0e0c0c "dra7xx: mmc: Enable DDR50 mode for MMC1"
Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

drivers/mmc/omap_hsmmc.c
@@ -804,6 +804,15 @@ int omap_mmc_init(int dev_index, uint host_caps_mask, uint f_max, int cd_gpio,
 	switch (dev_index) {
 	case 0:
 		priv_data->base_addr = (struct hsmmc *)OMAP_HSMMC1_BASE;
+#if (defined(CONFIG_DRA7XX) || defined(CONFIG_AM57XX))
+#if defined(CONFIG_OMAP_MMC1_HS_52)
+		host_caps_val = MMC_MODE_HC | MMC_MODE_4BIT |
+				MMC_MODE_HS_52MHz;
+#else
+		host_caps_val = MMC_MODE_HC | MMC_MODE_4BIT |
+				MMC_MODE_HS;
+#endif /*CONFIG_OMAP_MMC1_HS_52 */
+#endif
 		break;
 #ifdef OMAP_HSMMC2_BASE
 	case 1:
@@ -813,7 +822,16 @@ int omap_mmc_init(int dev_index, uint host_caps_mask, uint f_max, int cd_gpio,
 		defined(CONFIG_HSMMC2_8BIT)
 		/* Enable 8-bit interface for eMMC on OMAP4/5 or DRA7XX */
 		host_caps_val |= MMC_MODE_8BIT;
-		host_caps_val |= MMC_MODE_DDR_52MHz;
+#endif
+
+#if (defined(CONFIG_DRA7XX) || defined(CONFIG_AM57XX))
+#if defined(CONFIG_OMAP_MMC2_DDR_52)
+		host_caps_val = MMC_MODE_HC | MMC_MODE_8BIT |
+				MMC_MODE_DDR_52MHz;
+#else
+		host_caps_val = MMC_MODE_HC | MMC_MODE_8BIT |
+				MMC_MODE_HS;
+#endif /* CONFIG_OMAP_MMC2_DDR_52 */
 #endif
 		break;
 #endif