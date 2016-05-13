3e4cdb3c8c359e577493d3f965e7138569e9ed36
Sam Protsenko <semen.protsenko@linaro.org>
2016-05-13 18:19:52 +0300

    arm: am57xx: Define Android partition table
    "fastboot oem format" command reuses "gpt write" command, which in turn
    requires correct partitions defined in $partitions variable. This patch
    adds such definition of Android partitions for DRA7XX EVM board.
    
    By default $partitions variable contains Linux partition table. In order
    to prepare Android environment one can run next commands from U-Boot
    shell:
    
        => env set partitions $partitions_android
        => env save
    
    After those operations one can go to fastboot mode and perform
    "fastboot oem format" to create Android partition table.
    
    While at it, enable CONFIG_RANDOM_UUID to spare user from providing
    UUIDs for each partition manually.
    
    Change-Id: I8838c02b6f2603503c9bffa13de4b8ed7d8a9239
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- include/configs/am57xx_evm.h
+++ include/configs/am57xx_evm.h
@@ -34,16 +34,36 @@
 
 #define CONFIG_SYS_OMAP_ABE_SYSCK
 
+#ifndef CONFIG_SPL_BUILD
 /* Define the default GPT table for eMMC */
 #define PARTS_DEFAULT \
+	/* Linux partitions */ \
 	"uuid_disk=${uuid_gpt_disk};" \
-	"name=rootfs,start=2MiB,size=-,uuid=${uuid_gpt_rootfs}"
+	"name=rootfs,start=2MiB,size=-,uuid=${uuid_gpt_rootfs}\0" \
+	/* Android partitions */ \
+	"partitions_android=" \
+	"uuid_disk=${uuid_gpt_disk};" \
+	"name=xloader,start=128K,size=128K,uuid=${uuid_gpt_xloader};" \
+	"name=bootloader,size=384K,uuid=${uuid_gpt_bootloader};" \
+	"name=environment,size=128K,uuid=${uuid_gpt_environment};" \
+	"name=misc,size=128K,uuid=${uuid_gpt_misc};" \
+	"name=efs,start=1280K,size=16M,uuid=${uuid_gpt_efs};" \
+	"name=crypto,size=16K,uuid=${uuid_gpt_crypto};" \
+	"name=recovery,size=10M,uuid=${uuid_gpt_recovery};" \
+	"name=boot,size=10M,uuid=${uuid_gpt_boot};" \
+	"name=system,size=768M,uuid=${uuid_gpt_system};" \
+	"name=cache,size=256M,uuid=${uuid_gpt_cache};" \
+	"name=ipu1,size=1M,uuid=${uuid_gpt_ipu1};" \
+	"name=ipu2,size=1M,uuid=${uuid_gpt_ipu2};" \
+	"name=userdata,size=-,uuid=${uuid_gpt_userdata}"
+#endif
 
 #include <configs/ti_omap5_common.h>
 
 /* Enhance our eMMC support / experience. */
 #define CONFIG_CMD_GPT
 #define CONFIG_EFI_PARTITION
+#define CONFIG_RANDOM_UUID
 
 /* CPSW Ethernet */
 #define CONFIG_BOOTP_DNS		/* Configurable parts of CMD_DHCP */
